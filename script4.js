561556dd07a09ecc258c30ccb2429ecafa55e048
Eric Furukawa <ericfurukawa@Erics-MacBook-Pro.local>
2021-10-24 11:04:02 -0700

    added prompts for numbers and symbols

--- script4.js
+++ script4.js
@@ -23,47 +23,48 @@ function generatePassword() {
     var charValues = []
 
     var lower = prompt("Would you like to include lowercase? Type yes or no");
-    if (lower == "yes") {
-        charValues.push("lower");
+    switch(lower) {
+        case "yes": charValues.push("lower");
+        break;
+        case "no": ""
+        break;
+        default: window.prompt("Please enter a valid response.");
+        break;
     }
+    // if (lower == "yes") {
+    //     charValues.push("lower");
+    // } else {
+    //     alert("Please enter a valid response.")
+
+    // return generatePassword();
+    // }
 
   var upper = prompt("Would you like to include uppercase? Type yes or no");
     if (upper == "yes") {
         charValues.push("upper");
+    } else {
+        alert("Please enter a valid response.")
+
+    return generatePassword();
     }
 
     var numbers = prompt("Would you like to include numbers? Type yes or no");
     if (numbers == "yes") {
         charValues.push("numbers");
+    } else {
+        alert("Please enter a valid response.")
+
+    return generatePassword();
     }
 
     var symbols = prompt("Would you like to include symbols? Type yes or no");
     if (symbols == "yes") {
         charValues.push("symbols");
+    } else {
+        alert("Please enter a valid response.")
+
+    return generatePassword();
     }
-
-
- 
-//   var number = prompt("Would you like to include numbers? Type yes or no");
-//     if (number == "yes") {
-//       var number = true;
-//     }
-//   console.log(number);
-
-//   var symbol = prompt("Would you like to include symbols? Type yes or no");
-//     if (symbol == "yes") {
-//       var symbol = true;
-//     }
-//   console.log(symbol);
-    
-    // var lower = prompt("Would you like to include lowercase? Type yes or no");
-    // if (lower == "yes") {
-    //   var lower = true;
-    // } else if (lower == "no") {
-    //     var lower = false;
-    // } else {
-    //     alert("Please enter a valid response.")
-    // }
 
     var characters = ""
     for (i = 0; i < length; i++) {
@@ -71,9 +72,14 @@ function generatePassword() {
             if (charValues[arrayIndex] === "lower") { 
                 // use lower
                 characters += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-            } else { 
+            } else if (charValues[arrayIndex] === "upper") { 
                 // use upper
                 characters += String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+            } else if (charValues[arrayIndex] === "numbers") {
+                // use numbers
+                characters += String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+            } else {
+                characters += String.fromCharCode(Math.floor(Math.random() * 15) + 33);
             }
         
     }
